23d323a1e17b7b0344a8ad0a5733f81099796c21
artembayan <babaian.artem@yandex.ru>
2020-05-03 17:52:02 +0400

    Third Commit

--- index.php
+++ index.php
@@ -8,16 +8,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Админ-панель</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-</br>
+
+
+<br/>
+
 <div class="container">
-<ul class="nav nav-tabs">
+<ul class="nav nav-tabs" id="MyTabs">
   <li class="nav-item">
     <a class="nav-link active" data-toggle="tab" href="#Products">Продукты</a>
   </li>
@@ -40,28 +45,29 @@
     <a class="nav-link" data-toggle="tab" href="#Requisites">Реквизиты</a>
   </li>
 </ul>
-</br>
+<br/>
 </div>
 
 <?php
-include('ConnectDB.php');
+include('./ConnectDB.php');
 
 function redirect_to($new_location) {//функция для отмены повторной отправки формы
 header("Location: " . $new_location);//(редирект)
 exit();
 }
 
-ini_set('display_errors',1);
-error_reporting(E_ALL);
 ?> 
 
-<div class="tab-content">
-  <div class="tab-pane active container" id="Products"><?php include('Products.php');?></div>
+<div class="tab-content" id="Tabs">
+  <div class="tab-pane active container" id="Products"><?php include('app/Products.php'); //Подключение файла с готовыми изделиями?></div>
   <div class="tab-pane container" id="Services"><?php include('Services.php'); //Подключение файла с улугами ?></div>
   <div class="tab-pane container" id="Staff"><?php include('Staff.php'); //Подключение файла с сотрудниками ?></div>
   <div class="tab-pane container" id="Courses"><?php include('Courses.php'); //Подключение файла со курсами ?></div>
   <div class="tab-pane container" id="Tracking"><?php include('Tracking.php'); // Подключение файла с отслеживанием заказов ?></div>
   <div class="tab-pane container" id="Working_hours"><?php include('Working_hours.php'); // Подключение файла с режимом работы ?></div>
   <div class="tab-pane container" id="Requisites"><?php include('Requisites.php'); // Подключение файла с реквизитами ?></div>  
+
+
+
 </body>
 </html>
